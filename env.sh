1b12be20b94d0f24757370d63d360209a8552793
b1f6c1c4 <b1f6c1c4@gmail.com>
2020-01-14 15:40:49 -0500

    WIP: restructure

---
+++ env.sh
@@ -0,0 +1,23 @@
+export GOPATH="$PWD"
+
+go get github.com/DisposaBoy/JsonConfigReader
+go get github.com/gorilla/handlers
+go get github.com/gorilla/websocket
+go get golang.org/x/crypto/acme/autocert
+go get golang.org/x/net/context
+go get google.golang.org/grpc
+go get google.golang.org/grpc/credentials
+go get google.golang.org/grpc/keepalive
+go get firebase.google.com/go
+go get firebase.google.com/go/messaging
+go get github.com/aws/aws-sdk-go/aws
+go get github.com/aws/aws-sdk-go/aws/awserr
+go get github.com/aws/aws-sdk-go/aws/credentials
+go get github.com/aws/aws-sdk-go/aws/request
+go get github.com/aws/aws-sdk-go/aws/session
+go get github.com/aws/aws-sdk-go/service/s3
+go get github.com/aws/aws-sdk-go/service/s3/s3manager
+go get github.com/tinode/snowflake
+go get golang.org/x/oauth2/google
+go get google.golang.org/api/option
+go get gopkg.in/rethinkdb/rethinkdb-go.v5
