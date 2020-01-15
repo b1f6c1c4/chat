e3016c1d8b059b94b10d7db43dc2e3e4c0fa2d29
b1f6c1c4 <b1f6c1c4@gmail.com>
2020-01-15 13:25:38 -0500

    some changes

--- nginx/src/main.js
+++ nginx/src/main.js
@@ -1,43 +1,78 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
+import { Provider, connect } from 'react-redux';
 import { applyMiddleware, createStore, compose } from 'redux';
+import { ConnectedRouter, routerMiddleware } from 'connected-react-router/immutable';
 import createSagaMiddleware from 'redux-saga';
 import persistState from 'redux-localstorage';
-import { fromJS, Set } from 'immutable';
-import * as datefns from 'date-fns';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import { createStructuredSelector, createSelector } from 'reselect';
+import { createBrowserHistory } from 'history';
+import { fromJS } from 'immutable';
+import { createMuiTheme, CssBaseline, MuiThemeProvider } from '@material-ui/core';
+import { brown, teal } from '@material-ui/core/colors';
+import { Switch, Route } from 'react-router';
 import Root from './containers/Root';
-import * as actions from './actions';
-import reducers from './reducers';
+import createReducer from './reducers';
 import rootSaga from './sagas';
 import './main.css';
+
+
+const fonts = {
+  en: '"Roboto", "Helvetica", "Arial", sans-serif',
+  zh: '"Noto Sans SC X", "Noto Sans SC", "Microsoft YaHei", sans-serif',
+};
+
+const makeTheme = (fontFamily) => createMuiTheme({
+  typography: {
+    fontFamily,
+  },
+  palette: {
+    primary: {
+      light: teal[600],
+      main: teal[800],
+      dark: teal[900],
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: brown[600],
+      main: brown[800],
+      dark: brown[900],
+      contrastText: '#fff',
+    },
+  },
+});
+
+const ConnectedMuiThemeProvider = connect(createStructuredSelector({
+  theme: createSelector(
+    (state) => state.getIn(['language', 'locale']),
+    (state) => makeTheme(fonts[state]),
+  ),
+}))(MuiThemeProvider);
+
+const history = createBrowserHistory();
 
 const saga = createSagaMiddleware();
 
 const middlewares = [
   saga,
+  routerMiddleware(history),
 ];
 
 const enhancers = [
   applyMiddleware(...middlewares),
   persistState(undefined, {
-    key: 'b1cal',
+    key: 'chat',
     serialize: (state) => JSON.stringify({
-      start: datefns.format(state.getIn(['view', 'start']), 'yyyy-MM-dd'),
-      end: datefns.format(state.getIn(['view', 'end']), 'yyyy-MM-dd'),
-      config: state.get('config').toJS(),
+      jwt: state.get(['global', 'jwt']),
     }),
     deserialize: (raw) => {
       const parsed = JSON.parse(raw);
       if (!parsed) {
         return undefined;
       }
-      const { start, end, config } = parsed;
+      const { jwt } = parsed;
       return fromJS({
-        start: datefns.parseISO(start),
-        end: datefns.parseISO(end),
-        config,
+        global: { jwt },
       });
     },
     merge: (init, states) => init.mergeDeep(states),
@@ -48,42 +83,31 @@ const enhancers = [
 const composeEnhancers = process.env.NODE_ENV !== 'production'
   && typeof window === 'object'
   && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ name: 'b1cal', trace: true })
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ name: 'chat', trace: true })
   : compose;
 /* eslint-enable no-underscore-dangle */
 
-const today = datefns.parseISO(datefns.format(new Date(), 'yyyy-MM-dd'));
-const init = fromJS({
-  events: { },
-  view: {
-    start: today,
-    end: datefns.addDays(today, 7),
-  },
-  config: {
-    shift: 4,
-  },
-  command: 'scroll',
-})
-  .set('dirty', new Set())
-  .set('clean', new Set())
-  .set('selection', new Set());
+const init = fromJS({});
 
 const store = createStore(
-  reducers,
+  createReducer(history),
   init,
   composeEnhancers(...enhancers),
 );
 
 saga.run(rootSaga);
 
-store.dispatch(actions.updateView({
-  start: init.getIn(['view', 'start']),
-  end: init.getIn(['view', 'end']),
-}));
-
 render((
   <Provider store={store}>
     <CssBaseline />
-    <Root />
+    <ConnectedMuiThemeProvider>
+      <ConnectedRouter history={history}>
+        <Switch>
+          <Route path="/login" component={Root} />
+          <Route path="/seminar" component={Root} />
+          <Route path="/proposal" component={Root} />
+        </Switch>
+      </ConnectedRouter>
+    </ConnectedMuiThemeProvider>
   </Provider>
 ), document.getElementById('app'));
